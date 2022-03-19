40f83645a6954d16d1db88b3e0bfeabf8d0f1fd9
Lukin Egor <lukin.net@gmail.com>
2022-03-19 12:05:59 +0400

    wp

--- src/rss/tg/sync.clj
+++ src/rss/tg/sync.clj
@@ -1,14 +1,33 @@
 (ns rss.tg.sync
   (:use pl.danieljanus.tagsoup))
 
-(use 'pl.danieljanus.tagsoup)
-;; (fetch-post-by-id "addmeto" 4789)
-;; (fetch-post-by-id "addmeto" 0)
+;; (find-last-post "addmeto")
 
 (def tg-url "https://t.me")
-
 (def intro-post-id 0)
 (def started-post-id 100000)
+(def meta-content-property "twitter:description")
+
+(defn find-last-post [channel]
+  (let [intro (fetch-post-by-id channel intro-post-id)]
+    (find-last-post-iter channel
+                         intro
+                         (/ started-post-id 2)
+                         started-post-id)))
+
+(defn find-last-post-iter [channel intro step guess-id]
+  (let [current-post (fetch-post-by-id channel guess-id)
+        next-post (fetch-post-by-id channel (+ guess-id 1))
+        next-post-2 (fetch-post-by-id channel (+ guess-id 2))]
+    (println step "-" guess-id)
+    (if (or
+         (last-post? current-post next-post next-post-2 intro)
+         (and (= step 1.0) (not (= current-post intro))))
+      { :id guess-id :content current-post }
+      (find-last-post-iter channel
+                              intro
+                              (Math/ceil (/ step 2))
+                              (next-guess-id guess-id step intro current-post)))))
 
 (defn fetch-post-by-id [channel post-id]
   (->>
@@ -26,53 +45,13 @@
 (defn twitter-meta-header? [item]
   (if (= (type item) clojure.lang.PersistentVector)
     (let [[key { name :name content :content }] item]
-      (and (= key :meta) (= name "twitter:description")))
+      (and (= key :meta) (= name meta-content-property)))
     false))
-
-;(89)
-; 100 ;50
-; 50 ;25
-; 75 ;13
-; 88 ;7
-; 95 ;4
-; 91 ;2
-; 89 ;1
-
-; intro = fetch(0)
-; step = started-post-id
-; start_post = fetch(started-post-id)
-; (= start_post intro)
-; last-post? current-port next-post intro-content
-; find-last-post-id-rec []
-
-
-
-(defn find-last-post [channel]
-  (let [intro (fetch-post-by-id channel intro-post-id)]
-    (find-last-post-iter channel
-                            intro
-                            (/ started-post-id 2)
-                            started-post-id)))
-
-(defn find-last-post-iter [channel intro step guess-id]
-  (let [current-post (fetch-post-by-id channel guess-id)
-        next-post (fetch-post-by-id channel (+ guess-id 1))]
-    (println guess-id)
-    (if (last-post? current-post next-post intro)
-      guess-id
-      (find-last-post-iter channel
-                              intro
-                              (Math/ceil (/ step 2))
-                              (next-guess-id guess-id step intro current-post)))))
 
 (defn next-guess-id [guess-id step intro current-post]
   (if (= current-post intro)
     (- guess-id step)
     (+ guess-id step)))
 
-(defn last-post? [current next intro]
-  (and (not (= current next)) (= next intro)))
-
-
-(find-last-post "addmeto")
-(find-last-post "oldlentach")
+(defn last-post? [current next next2 intro]
+  (and (not (= current next)) (= next intro) (= next2 intro)))
