6145b82411a34d0e96fbf7b5f1fb8f08909ac1e0
Lukin Egor <lukin.net@gmail.com>
2022-04-14 22:58:35 +0400

    add rss generator

--- src/rss/api/server.clj
+++ src/rss/api/server.clj
@@ -1,7 +1,8 @@
 (ns rss.api.server
-  (:require [rss.api.channels :as channels])
-  (:require [ring.middleware.json :refer [wrap-json-body wrap-json-response]])
-  (:require [compojure.core :refer :all]
+  (:require [rss.api.channels :as channels]
+            [rss.api.rss.channels :as rss-channels]
+            [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
+            [compojure.core :refer :all]
             [compojure.coercions :refer [as-int]]
             [compojure.handler :as handler]
             [compojure.route :as route]))
@@ -10,6 +11,7 @@
   (GET "/channels" [] (channels/index))
   (POST "/channels" [:as { { channel :channel } :body }] (channels/create channel))
   (DELETE "/channels/:id" [id :<< as-int] (channels/delete id))
+  (GET "/rss/channels/:id" [id :<< as-int] (rss-channels/show id))
   (route/resources "/")
   (route/not-found "<h1>Page not found</h1>"))
 
